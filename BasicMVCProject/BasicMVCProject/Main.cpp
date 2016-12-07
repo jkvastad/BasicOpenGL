8739e6775214c631764384d9aa315610f64a00f7
Johan Kvastad <jkvastad@gmail.com>
2016-12-07 14:24:03 +0100

    Added Library and DLL folder containing SDL2 and GLEW2. Added init file for OpenGL and SDL which when run creates and then closes a simple window.

---
+++ BasicMVCProject/BasicMVCProject/Main.cpp
@@ -0,0 +1,10 @@
+#include "Main.h"
+
+int main(int argc, char* argv[]) {
+	SDL_Window* main_window;
+	init(main_window);
+
+	tearDown(main_window);
+
+	return 0;
+}
---
+++ BasicMVCProject/BasicMVCProject/Main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+#include "GLInit.h"
+
+#endif
